6eb543b0b37e4c528ef3aae326c73b0f6b36db8c
Sinan Taifour <sinan@taifour.com>
2013-06-04 11:25:04 +0300

    Added a README.

--- lib/bitmessage/version.rb
+++ lib/bitmessage/version.rb
@@ -1,6 +1,6 @@
 module Bitmessage
   VERSION = '0.1.0'
-  COMPATIBLE = '0.3.1' # Compatible version of PyBitmessage.
+  COMPATIBLE = '0.3.1' # Compatible version of PyBitmessage (commit 7e9ac11).
   PROTOCOL_VERSION = 2
   STREAM = 1
 end
